a86948a7b1f89456cc50fabac2d47b09b5ecaa7b
dalobstah <joel@lobsta.dev>
2021-02-24 01:13:56 -0600

    Changed text in login link

--- client/pages/login.vue
+++ client/pages/login.vue
@@ -3,7 +3,7 @@
     <div class="title">Blimb</div>
     <input type="text" name="username" id="username" placeholder="Username" class="mb-4">
     <input type="password" name="password" id="password" placeholder="Password">
-    <NuxtLink class="btn btn-link mt-4" to="/signup">Forgot Username/Password</NuxtLink>
+    <NuxtLink class="btn btn-link mt-4" to="/signup">I need an account</NuxtLink>
     <button class="btn btn-primary mt-4">Login</button>
   </div>
 </template>
