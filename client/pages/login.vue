2da633368c79aa7719a4dca551cb0134d1cd89f6
dalobstah <joel@lobsta.dev>
2021-02-24 01:09:48 -0600

    Login/signup pages

---
+++ client/pages/login.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="page-container">
+    <div class="title">Blimb</div>
+    <input type="text" name="username" id="username" placeholder="Username" class="mb-4">
+    <input type="password" name="password" id="password" placeholder="Password">
+    <NuxtLink class="btn btn-link mt-4" to="/signup">Forgot Username/Password</NuxtLink>
+    <button class="btn btn-primary mt-4">Login</button>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({})
+</script>
---
+++ client/pages/signup.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="page-container">
+    <div class="title">Blimb</div>
+    <input type="text" name="email" id="email" placeholder="Email" class="mb-4">
+    <input type="text" name="username" id="username" placeholder="Username" class="mb-4">
+    <input type="password" name="password" id="password" placeholder="Password">
+    <NuxtLink class="btn btn-link mt-4" to="/login">I already have an account</NuxtLink>
+    <button class="btn btn-primary mt-4">Create Account</button>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({})
+</script>
