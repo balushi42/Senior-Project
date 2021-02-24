8b0ff936ca39a452231a65117dd9fb747be2afd5
Isam Al Balushi <thebalushi42@gmail.com>
2021-02-24 16:00:45 -0600

    rank based search(preparation for suggestion system), fixed file upload location, implemented basic search interface

--- blimp/library/views.py
+++ blimp/library/views.py
@@ -1,15 +1,38 @@
 from django.shortcuts import render
+from django import template
 from library.models import *
 from django.contrib import messages
+from django.http import HttpResponse, JsonResponse
+from django.db.models import Count, Q, F, Value, IntegerField, Case, When, OuterRef, Subquery
+
+
+import re
 
 def home(request):
     if request.method == 'POST':
         file = request.FILES["file"]
+        title = request.POST.get("title")
 
         if file.name.endswith('.mp4'):
-            Video(file=file).save()
+            Video(file=file, title=title).save()
         else:
             messages.error(request,'File is not of video type')
+    elif request.method == 'GET' and request.GET.get('query'):
+    	#rank baed on search terms
+    	videos = Video.objects.annotate(rank=Value(0, IntegerField())) # setup with raking 0
+
+    	for term in re.split('\W+', request.GET.get('query')):
+    		videos = videos.annotate(rank=Case(When(title__icontains=term, then=F('rank')+Value(1, IntegerField())), default=F('rank'), output_field=IntegerField())) #update rank to add 1
+
+    	videos = videos.order_by('-rank')
+
+    	for v in videos:
+    		print(v.rank)
+
+    	resultsHTML = template.loader.render_to_string('include/video_list.html', {
+					'videos':videos,})
+
+    	return JsonResponse({'search-result':resultsHTML})
 
     videos = Video.objects.all()
 
